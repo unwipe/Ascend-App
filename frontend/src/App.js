5fde23eef86792a21cdf4e8a33c728367e527377
unwipe <ako.versweyveld@gmail.com>
2025-11-07 19:17:26 +0000

    Use NEW Google Client ID env to avoid stale secret reference

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1441,7 +1441,7 @@ function App() {
       
       {/* Welcome/Login Modal */}
       {showWelcome && (
-        <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+        <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID_NEW}>
           <WelcomeModal
             isOpen={showWelcome}
             onGoogleLogin={handleGoogleLogin}
